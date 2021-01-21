e5c6ccb6e6b3e4659af037f69ae0a12ca0e275c0
hyq07508 <hyq07508@ly.com>
2021-01-21 18:26:56 +0800

    添加一个空项目

---
+++ blank/src/main/java/org/example/blank/BlankApplication.java
@@ -0,0 +1,14 @@
+package org.example.blank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hyq on 2021/1/21 18:24.
+ */
+@SpringBootApplication
+public class BlankApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlankApplication.class, args);
+    }
+}
