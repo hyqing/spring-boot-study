cea8f0d10730423165ccbedcfdf32473e4041121
hyq07508 <hyq07508@ly.com>
2021-01-21 18:35:45 +0800

    添加Async项目

---
+++ async/src/main/java/org/example/aync/AsyncApplication.java
@@ -0,0 +1,14 @@
+package org.example.aync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by hyq on 2021/1/21 18:29.
+ */
+@SpringBootApplication
+public class AsyncApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncApplication.class, args);
+    }
+}
---
+++ async/src/note.md
@@ -0,0 +1,5 @@
+### 参考资料：
+
+* [Spring使用@Async注解](https://www.cnblogs.com/wlandwl/p/async.html)
+* [Spring中@Async用法总结](https://www.cnblogs.com/jpfss/p/10273129.html)
+* [Spring中异步注解@Async的使用、原理及使用时可能导致的问题](https://blog.51cto.com/14890701/2518888#h0)
